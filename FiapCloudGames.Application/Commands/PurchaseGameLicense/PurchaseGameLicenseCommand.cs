fbda23e9fa7f7464971d3d437f22266492132010
heroshg <heros.goodoy@gmail.com>
2026-01-17 17:18:39 -0300

    feat: removing user id in body request

--- FiapCloudGames.Application/Commands/PurchaseGameLicense/PurchaseGameLicenseCommand.cs
+++ FiapCloudGames.Application/Commands/PurchaseGameLicense/PurchaseGameLicenseCommand.cs
@@ -7,8 +7,6 @@ namespace FiapCloudGames.Application.Commands.PurchaseGameLicense
     public record PurchaseGameLicenseCommand(
         [Required(ErrorMessage = "Game id is required.")]
         Guid GameId,
-        [Required(ErrorMessage = "User id is required")]
-        Guid UserId,
         DateTime? ExpirationDate) : IRequest<ResultViewModel<Guid>>
     {
     }
--- FiapCloudGames.Application/Commands/PurchaseGameLicense/PurchaseGameLicenseHandler.cs
+++ FiapCloudGames.Application/Commands/PurchaseGameLicense/PurchaseGameLicenseHandler.cs
@@ -1,6 +1,7 @@
 ﻿using FiapCloudGames.Application.Models;
 using FiapCloudGames.Domain.Games;
 using FiapCloudGames.Domain.Identity.Repositories;
+using Microsoft.AspNetCore.Http;
 using NetDevPack.SimpleMediator;
 
 namespace FiapCloudGames.Application.Commands.PurchaseGameLicense
@@ -11,18 +12,30 @@ namespace FiapCloudGames.Application.Commands.PurchaseGameLicense
         private readonly IGameRepository _gameRepository;
         private readonly IGameLicenseRepository _gameLicenseRepository;
         private readonly IGamePurchaseService _purchaseService;
+        private readonly IHttpContextAccessor _accessor;
 
-        public PurchaseGameLicenseHandler(IUserRepository userRepository, IGameRepository gameRepository, IGameLicenseRepository gameLicenseRepository, IGamePurchaseService purchaseService)
+        public PurchaseGameLicenseHandler(IUserRepository userRepository, IGameRepository gameRepository, IGameLicenseRepository gameLicenseRepository, IGamePurchaseService purchaseService, IHttpContextAccessor accessor)
         {
             _userRepository = userRepository;
             _gameRepository = gameRepository;
             _gameLicenseRepository = gameLicenseRepository;
             _purchaseService = purchaseService;
+            _accessor = accessor;
         }
 
         public async Task<ResultViewModel<Guid>> Handle(PurchaseGameLicenseCommand request, CancellationToken cancellationToken)
         {
-            var user = await _userRepository.GetByIdAsync(request.UserId, cancellationToken);
+
+            var userIdClaim = _accessor.HttpContext?
+            .User
+            .FindFirst("userId")?.Value;
+
+            if (userIdClaim is null || !Guid.TryParse(userIdClaim, out var userId))
+            {
+                return ResultViewModel<Guid>.Error("PurchasePromotion failed: User ID claim is missing or invalid.");
+            }
+
+            var user = await _userRepository.GetByIdAsync(userId, cancellationToken);
             if(user is null)
             {
                 return ResultViewModel<Guid>.Error("User not found.");
